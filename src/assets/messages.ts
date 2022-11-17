2114bee0d0c8395453e06d3f51c92ec44d8ec406
caw <dominik.wozniewicz@gmail.com>
2022-11-17 15:28:29 +0100

    Skip button working

---
+++ src/assets/messages.ts
@@ -0,0 +1,45 @@
+export const breakMessages = [
+  {
+    id: 1,
+    text: 'Breaky breaky!',
+  },
+  {
+    id: 2,
+    text: 'Maybe go strech or something? :)',
+  },
+  {
+    id: 3,
+    text: 'Leave the computer alone NOW',
+  },
+  {
+    id: 4,
+    text: 'You really need to touch some grass',
+  },
+  {
+    id: 5,
+    text: "Don't you have anything better to do than staring at that screen?",
+  },
+];
+
+export const workMessages = [
+  {
+    id: 1,
+    text: 'Unfortunately you need to work right now. Good luck.',
+  },
+  {
+    id: 2,
+    text: 'Break a leg!',
+  },
+  {
+    id: 3,
+    text: 'Remember to stay focused.',
+  },
+  {
+    id: 4,
+    text: 'Do not give up',
+  },
+  {
+    id: 5,
+    text: 'FINISH THIS',
+  },
+];
--- src/timer/components/Timer.tsx
+++ src/timer/components/Timer.tsx
@@ -10,7 +10,8 @@ import { useDispatch } from 'react-redux/es/hooks/useDispatch';
 import { queryActions } from '../../store/slices/querySlice';
 import { timerActions } from '../../store/slices/timerSlice';
 import './Timer.css';
-import { messages } from '../../assets/BreakMessages';
+import { breakMessages, workMessages } from '../../assets/messages';
+import { BsFillSkipEndFill } from 'react-icons/bs';
 
 const Timer: FunctionComponent = () => {
   const INITIAL_WORK_TIME_IN_SECONDS = 25 * 60; // 25 minutes
@@ -25,11 +26,16 @@ const Timer: FunctionComponent = () => {
 
   const task = useSelector((state: any) => state.query.task);
   const intervalCount = useSelector((state: any) => state.timer.intervalCount);
-  const isBreakTime = useSelector((state: any) => state.timer.pomodoroState);
+  const pomodoroState = useSelector((state: any) => state.timer.pomodoroState);
 
   const randomBreakMessage = useMemo(
-    () => messages[Math.floor(Math.random() * 5)].text,
-    [isBreakTime]
+    () => breakMessages[Math.floor(Math.random() * 5)].text,
+    [pomodoroState]
+  );
+
+  const randomWorkMessage = useMemo(
+    () => workMessages[Math.floor(Math.random() * 5)].text,
+    [pomodoroState]
   );
 
   const activateTask = (interval: any) => {
@@ -45,62 +51,61 @@ const Timer: FunctionComponent = () => {
         setTimer((prevTime) => prevTime - 1);
       }, 1000);
 
-      if (timer === 1495 && intervalCount === 0) {
-        activateTask(interval);
-        setTimer(INITIAL_SHORT_BREAK_TIME_IN_SECONDS);
-        dispatch(timerActions.increaseIntervalCount());
-        dispatch(timerActions.changePomodoroState('shortBreak'));
-      }
-      console.log(intervalCount);
-      if (timer === 295 && intervalCount === 1) {
-        activateTask(interval);
-        setTimer(INITIAL_WORK_TIME_IN_SECONDS);
-        dispatch(timerActions.increaseIntervalCount());
-        dispatch(timerActions.changePomodoroState('work'));
-      }
-
-      if (timer === 1495 && intervalCount === 2) {
+      if (timer === 0 && intervalCount === 0) {
         activateTask(interval);
         setTimer(INITIAL_SHORT_BREAK_TIME_IN_SECONDS);
         dispatch(timerActions.increaseIntervalCount());
         dispatch(timerActions.changePomodoroState('shortBreak'));
       }
 
-      if (timer === 295 && intervalCount === 3) {
+      if (timer === 0 && intervalCount === 1) {
         activateTask(interval);
         setTimer(INITIAL_WORK_TIME_IN_SECONDS);
         dispatch(timerActions.increaseIntervalCount());
         dispatch(timerActions.changePomodoroState('work'));
       }
 
-      if (timer === 1495 && intervalCount === 4) {
+      if (timer === 0 && intervalCount === 2) {
         activateTask(interval);
         setTimer(INITIAL_SHORT_BREAK_TIME_IN_SECONDS);
         dispatch(timerActions.increaseIntervalCount());
         dispatch(timerActions.changePomodoroState('shortBreak'));
       }
 
-      if (timer === 295 && intervalCount === 5) {
+      if (timer === 0 && intervalCount === 3) {
         activateTask(interval);
         setTimer(INITIAL_WORK_TIME_IN_SECONDS);
         dispatch(timerActions.increaseIntervalCount());
         dispatch(timerActions.changePomodoroState('work'));
       }
 
-      if (timer === 1495 && intervalCount === 6) {
+      if (timer === 0 && intervalCount === 4) {
+        activateTask(interval);
+        setTimer(INITIAL_SHORT_BREAK_TIME_IN_SECONDS);
+        dispatch(timerActions.increaseIntervalCount());
+        dispatch(timerActions.changePomodoroState('shortBreak'));
+      }
+
+      if (timer === 0 && intervalCount === 5) {
+        activateTask(interval);
+        setTimer(INITIAL_WORK_TIME_IN_SECONDS);
+        dispatch(timerActions.increaseIntervalCount());
+        dispatch(timerActions.changePomodoroState('work'));
+      }
+
+      if (timer === 0 && intervalCount === 6) {
         activateTask(interval);
         setTimer(INITIAL_LONG_BREAK_TIME_IN_SECONDS);
         dispatch(timerActions.increaseIntervalCount());
         dispatch(timerActions.changePomodoroState('longBreak'));
       }
 
-      if (timer === 895 && intervalCount === 7) {
+      if (timer === 0 && intervalCount === 7) {
         activateTask(interval);
         setTimer(INITIAL_WORK_TIME_IN_SECONDS);
         dispatch(timerActions.resetIntervalCount());
         dispatch(timerActions.changePomodoroState('work'));
       }
-      console.log(intervalCount);
 
       return () => {
         clearInterval(interval);
@@ -111,13 +116,11 @@ const Timer: FunctionComponent = () => {
   const minutes = useMemo(() => Math.floor(timer / 60), [timer]);
   const seconds = useMemo(() => timer % 60, [timer]);
 
-  // const previousPomodoroState = () => {
-  //   setIntervalCount((prevIntervalCount) => prevIntervalCount - 1);
-  // };
-
-  // const skipCurrentPomodoroState = () => {
-  //   setIntervalCount((prevIntervalCount) => prevIntervalCount + 1);
-  // };
+  const skipTimerHandler = () => {
+    if (active && taskIsAccepted) {
+      setTimer(0);
+    }
+  };
 
   return (
     <Fragment>
@@ -134,15 +137,19 @@ const Timer: FunctionComponent = () => {
       />
       <h1 className="task">{active && taskIsAccepted && task}</h1>
       <h1>
-        {(isBreakTime === 'shortBreak' || isBreakTime === 'longBreak') &&
+        {(pomodoroState === 'shortBreak' || pomodoroState === 'longBreak') &&
           randomBreakMessage}
       </h1>
+      <h1>{pomodoroState === 'work' && randomWorkMessage}</h1>
       <h1 className="timer">
         {String(minutes).padStart(2, '0')}:{String(seconds).padStart(2, '0')}
+        {active && taskIsAccepted && (
+          <BsFillSkipEndFill
+            className="skip-button"
+            onClick={skipTimerHandler}
+          />
+        )}
       </h1>
-      <div style={{ marginTop: '-35px' }}>
-        <button>PREV</button> <button>SKIP</button>
-      </div>
     </Fragment>
   );
 };
